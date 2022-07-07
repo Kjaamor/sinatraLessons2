0f79e2c77348f476f747b79a2824662687628465
James O'Keefe <james.okeefe@Jamess-MBP.fritz.box>
2022-07-07 15:58:25 +0100

    Tidy up code in .rb file.

--- sinatra_me.rb
+++ sinatra_me.rb
@@ -1,20 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
-# get '/:name?' do |name|
-#     erb :index, :locals => {:name=>name}
-# end
-
 get '/index' do
     @cell_choice = params["cell_choice"]
     erb :index
 end
-
-# post '/index' do
-#     get top_right_x do
-#         if params[:top-right]
-#             erb :index, :locals => {name=>name + "!!!!"}
-#             return "Success"
-#         end
-#     end
-# end
