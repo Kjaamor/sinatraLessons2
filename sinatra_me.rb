f8cd7a2896f1ec89c4db0a5f21409007190ab950
James O'Keefe <james.okeefe@Jamess-MBP.fritz.box>
2022-07-07 14:50:11 +0100

    Remove and comment out superfluous methods that were causing program to fail. Include radio form as buttons were not receiving inputs.

--- sinatra_me.rb
+++ sinatra_me.rb
@@ -1,26 +1,20 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
+# get '/:name?' do |name|
+#     erb :index, :locals => {:name=>name}
+# end
 
-get '/' do
-    "HammerTime test"
+get '/index' do
+    @cell_choice = params["cell_choice"]
+    erb :index
 end
 
-get '/:name?' do |name|
-    erb :index, :locals => {:name=>name}
-end
-
-get '/mathstest' do
-    if 1+1=2
-        "1 plus 1 equals two!"
-    end
-end
-
-post "/index" do
-    get top_right_x do
-        if params[:top-right]
-            erb :index, :locals => {name=>name + "!!!!"}
-            return "Success"
-        end
-    end
-end
+# post '/index' do
+#     get top_right_x do
+#         if params[:top-right]
+#             erb :index, :locals => {name=>name + "!!!!"}
+#             return "Success"
+#         end
+#     end
+# end
